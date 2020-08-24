63b9fec54768dc9891bb63cbd4f5c1ba01a63b0c
distant.zoomer <distantzoomer@protonmail.com>
2020-08-23 23:30:36 -0400

    Adding the other raw wav github pages link

--- scripts/sound.js
+++ scripts/sound.js
@@ -7,15 +7,19 @@ let helloBasedButton = document.getElementById( 'hello-based-button' )
 // BASED
 basedSoundButton.addEventListener( 'click', function() {
   // let sound = new Audio( '../sounds/based.wav' )
+
+  // Necessary for github pages to work
   let sound = new Audio( 'https://raw.githubusercontent.com/distant-zoomer/fuentes-based-app/master/sounds/based.wav' )
-  // https://raw.githubusercontent.com/myName/myRepo/master/vendor/assets/music/Tetris.mp3
   sound.play()
   sound.currentTime = 0
 }, false )
 
 // Hello? Based?
 helloBasedButton.addEventListener( 'click', function() {
-  let sound = new Audio( '../sounds/based-department.wav' )
+  // let sound = new Audio( '../sounds/based-department.wav' )
+  
+  // Necessary for github pages to work
+  let sound = new Audio( 'https://raw.githubusercontent.com/distant-zoomer/fuentes-based-app/master/sounds/based-department.wav' )
   sound.play()
   sound.currentTime = 0
 }, false )
