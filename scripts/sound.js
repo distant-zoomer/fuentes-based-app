72c6f220d2dbf12d2a7481e58cc2fcd46dcd0e42
distant.zoomer <distantzoomer@protonmail.com>
2020-08-23 23:17:01 -0400

    Changing sound path for github pages test

--- scripts/sound.js
+++ scripts/sound.js
@@ -6,7 +6,9 @@ let helloBasedButton = document.getElementById( 'hello-based-button' )
 
 // BASED
 basedSoundButton.addEventListener( 'click', function() {
-  let sound = new Audio( '../sounds/based.wav' )
+  // let sound = new Audio( '../sounds/based.wav' )
+  let sound = new Audio( 'https://raw.githubusercontent.com/distant-zoomer/fuentes-based-app/master/sounds/based.wav' )
+  // https://raw.githubusercontent.com/myName/myRepo/master/vendor/assets/music/Tetris.mp3
   sound.play()
   sound.currentTime = 0
 }, false )
